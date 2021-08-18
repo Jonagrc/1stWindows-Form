7308c358c59b6405b1348878a9946af23960732c
Jonagrc <77440105+Jonagrc@users.noreply.github.com>
2021-08-18 07:48:07 -0700

    Initial Commit of windows Forms

---
+++ Week6Day2EX6.2/Form1.Designer.cs
@@ -0,0 +1,278 @@
+﻿
+namespace EmpdbSystem
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.gridviewEmp = new System.Windows.Forms.DataGridView();
+            this.lblempid = new System.Windows.Forms.Label();
+            this.lbldptid = new System.Windows.Forms.Label();
+            this.lbllname = new System.Windows.Forms.Label();
+            this.lblfname = new System.Windows.Forms.Label();
+            this.lbldeg = new System.Windows.Forms.Label();
+            this.lblsal = new System.Windows.Forms.Label();
+            this.txtboxEmpId = new System.Windows.Forms.TextBox();
+            this.txtboxFname = new System.Windows.Forms.TextBox();
+            this.txtboxLname = new System.Windows.Forms.TextBox();
+            this.txtboxSalary = new System.Windows.Forms.TextBox();
+            this.cmbboxDeptId = new System.Windows.Forms.ComboBox();
+            this.cmbboxdesig = new System.Windows.Forms.ComboBox();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnRefresh = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.btnUpdate = new System.Windows.Forms.Button();
+            this.btnEdit = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.gridviewEmp)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // gridviewEmp
+            // 
+            this.gridviewEmp.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.gridviewEmp.Location = new System.Drawing.Point(79, 50);
+            this.gridviewEmp.Name = "gridviewEmp";
+            this.gridviewEmp.Size = new System.Drawing.Size(609, 150);
+            this.gridviewEmp.TabIndex = 0;
+            // 
+            // lblempid
+            // 
+            this.lblempid.AutoSize = true;
+            this.lblempid.Font = new System.Drawing.Font("Sitka Subheading", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblempid.Location = new System.Drawing.Point(76, 203);
+            this.lblempid.Name = "lblempid";
+            this.lblempid.Size = new System.Drawing.Size(113, 28);
+            this.lblempid.TabIndex = 1;
+            this.lblempid.Text = "Employee Id";
+            // 
+            // lbldptid
+            // 
+            this.lbldptid.AutoSize = true;
+            this.lbldptid.Font = new System.Drawing.Font("Sitka Subheading", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbldptid.Location = new System.Drawing.Point(74, 324);
+            this.lbldptid.Name = "lbldptid";
+            this.lbldptid.Size = new System.Drawing.Size(133, 28);
+            this.lbldptid.TabIndex = 2;
+            this.lbldptid.Text = "Department Id";
+            // 
+            // lbllname
+            // 
+            this.lbllname.AutoSize = true;
+            this.lbllname.Font = new System.Drawing.Font("Sitka Subheading", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbllname.Location = new System.Drawing.Point(74, 283);
+            this.lbllname.Name = "lbllname";
+            this.lbllname.Size = new System.Drawing.Size(186, 28);
+            this.lbllname.TabIndex = 3;
+            this.lbllname.Text = "Employee Last Name";
+            // 
+            // lblfname
+            // 
+            this.lblfname.AutoSize = true;
+            this.lblfname.Font = new System.Drawing.Font("Sitka Subheading", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblfname.Location = new System.Drawing.Point(76, 244);
+            this.lblfname.Name = "lblfname";
+            this.lblfname.Size = new System.Drawing.Size(190, 28);
+            this.lblfname.TabIndex = 4;
+            this.lblfname.Text = "Employee First Name";
+            // 
+            // lbldeg
+            // 
+            this.lbldeg.AutoSize = true;
+            this.lbldeg.Font = new System.Drawing.Font("Sitka Subheading", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbldeg.Location = new System.Drawing.Point(76, 363);
+            this.lbldeg.Name = "lbldeg";
+            this.lbldeg.Size = new System.Drawing.Size(113, 28);
+            this.lbldeg.TabIndex = 5;
+            this.lbldeg.Text = "Designation";
+            // 
+            // lblsal
+            // 
+            this.lblsal.AutoSize = true;
+            this.lblsal.Font = new System.Drawing.Font("Sitka Subheading", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblsal.Location = new System.Drawing.Point(76, 400);
+            this.lblsal.Name = "lblsal";
+            this.lblsal.Size = new System.Drawing.Size(65, 28);
+            this.lblsal.TabIndex = 6;
+            this.lblsal.Text = "Salary";
+            // 
+            // txtboxEmpId
+            // 
+            this.txtboxEmpId.Font = new System.Drawing.Font("Sitka Subheading", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtboxEmpId.Location = new System.Drawing.Point(306, 211);
+            this.txtboxEmpId.Name = "txtboxEmpId";
+            this.txtboxEmpId.Size = new System.Drawing.Size(100, 21);
+            this.txtboxEmpId.TabIndex = 7;
+            // 
+            // txtboxFname
+            // 
+            this.txtboxFname.Font = new System.Drawing.Font("Sitka Subheading", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtboxFname.Location = new System.Drawing.Point(306, 250);
+            this.txtboxFname.Name = "txtboxFname";
+            this.txtboxFname.Size = new System.Drawing.Size(100, 21);
+            this.txtboxFname.TabIndex = 8;
+            // 
+            // txtboxLname
+            // 
+            this.txtboxLname.Font = new System.Drawing.Font("Sitka Subheading", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtboxLname.Location = new System.Drawing.Point(306, 290);
+            this.txtboxLname.Name = "txtboxLname";
+            this.txtboxLname.Size = new System.Drawing.Size(100, 21);
+            this.txtboxLname.TabIndex = 9;
+            // 
+            // txtboxSalary
+            // 
+            this.txtboxSalary.Font = new System.Drawing.Font("Sitka Subheading", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtboxSalary.Location = new System.Drawing.Point(306, 400);
+            this.txtboxSalary.Name = "txtboxSalary";
+            this.txtboxSalary.Size = new System.Drawing.Size(100, 21);
+            this.txtboxSalary.TabIndex = 10;
+            // 
+            // cmbboxDeptId
+            // 
+            this.cmbboxDeptId.FormattingEnabled = true;
+            this.cmbboxDeptId.Location = new System.Drawing.Point(306, 330);
+            this.cmbboxDeptId.Name = "cmbboxDeptId";
+            this.cmbboxDeptId.Size = new System.Drawing.Size(121, 21);
+            this.cmbboxDeptId.TabIndex = 11;
+            // 
+            // cmbboxdesig
+            // 
+            this.cmbboxdesig.FormattingEnabled = true;
+            this.cmbboxdesig.Location = new System.Drawing.Point(306, 363);
+            this.cmbboxdesig.Name = "cmbboxdesig";
+            this.cmbboxdesig.Size = new System.Drawing.Size(121, 21);
+            this.cmbboxdesig.TabIndex = 12;
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Font = new System.Drawing.Font("Sitka Heading", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAdd.Location = new System.Drawing.Point(483, 231);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(136, 40);
+            this.btnAdd.TabIndex = 13;
+            this.btnAdd.Text = "Add New Record";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // btnRefresh
+            // 
+            this.btnRefresh.Font = new System.Drawing.Font("Sitka Heading", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnRefresh.Location = new System.Drawing.Point(652, 231);
+            this.btnRefresh.Name = "btnRefresh";
+            this.btnRefresh.Size = new System.Drawing.Size(136, 40);
+            this.btnRefresh.TabIndex = 14;
+            this.btnRefresh.Text = "Refresh";
+            this.btnRefresh.UseVisualStyleBackColor = true;
+            this.btnRefresh.Click += new System.EventHandler(this.btnRefresh_Click);
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Font = new System.Drawing.Font("Sitka Heading", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDelete.Location = new System.Drawing.Point(483, 299);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(136, 40);
+            this.btnDelete.TabIndex = 15;
+            this.btnDelete.Text = "Delete Record";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // btnUpdate
+            // 
+            this.btnUpdate.Font = new System.Drawing.Font("Sitka Heading", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnUpdate.Location = new System.Drawing.Point(652, 299);
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Size = new System.Drawing.Size(136, 40);
+            this.btnUpdate.TabIndex = 16;
+            this.btnUpdate.Text = "Update";
+            this.btnUpdate.UseVisualStyleBackColor = true;
+            this.btnUpdate.Click += new System.EventHandler(this.btnUpdate_Click);
+            // 
+            // btnEdit
+            // 
+            this.btnEdit.Font = new System.Drawing.Font("Sitka Heading", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnEdit.Location = new System.Drawing.Point(568, 363);
+            this.btnEdit.Name = "btnEdit";
+            this.btnEdit.Size = new System.Drawing.Size(136, 40);
+            this.btnEdit.TabIndex = 17;
+            this.btnEdit.Text = "Select to Edit";
+            this.btnEdit.UseVisualStyleBackColor = true;
+            this.btnEdit.Click += new System.EventHandler(this.btnEdit_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnEdit);
+            this.Controls.Add(this.btnUpdate);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnRefresh);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.cmbboxdesig);
+            this.Controls.Add(this.cmbboxDeptId);
+            this.Controls.Add(this.txtboxSalary);
+            this.Controls.Add(this.txtboxLname);
+            this.Controls.Add(this.txtboxFname);
+            this.Controls.Add(this.txtboxEmpId);
+            this.Controls.Add(this.lblsal);
+            this.Controls.Add(this.lbldeg);
+            this.Controls.Add(this.lblfname);
+            this.Controls.Add(this.lbllname);
+            this.Controls.Add(this.lbldptid);
+            this.Controls.Add(this.lblempid);
+            this.Controls.Add(this.gridviewEmp);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.gridviewEmp)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView gridviewEmp;
+        private System.Windows.Forms.Label lblempid;
+        private System.Windows.Forms.Label lbldptid;
+        private System.Windows.Forms.Label lbllname;
+        private System.Windows.Forms.Label lblfname;
+        private System.Windows.Forms.Label lbldeg;
+        private System.Windows.Forms.Label lblsal;
+        private System.Windows.Forms.TextBox txtboxEmpId;
+        private System.Windows.Forms.TextBox txtboxFname;
+        private System.Windows.Forms.TextBox txtboxLname;
+        private System.Windows.Forms.TextBox txtboxSalary;
+        private System.Windows.Forms.ComboBox cmbboxDeptId;
+        private System.Windows.Forms.ComboBox cmbboxdesig;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnRefresh;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.Button btnUpdate;
+        private System.Windows.Forms.Button btnEdit;
+    }
+}
+
---
+++ Week6Day2EX6.2/Form1.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EmpdbSystem
+{
+    public partial class Form1 : Form
+    {
+        PCAD2EmployeeEntities1 entities; //need to make an instance of the dbo which being inherited on context.cs
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            entities = new PCAD2EmployeeEntities1();
+            gridviewEmp.DataSource = entities.Emptables.ToList(); // here we are getting all data from emptables and attaching to grid view datasource
+            //                                 ^^ converting emptables to a list to attach to the grid view 
+            btnUpdate.Enabled = false; //button is not able to click until true
+        }
+        private void ClearDataNew()
+        {
+            //clears data and gives options to enter new data
+            txtboxEmpId.Clear();
+            txtboxEmpId.ReadOnly = true;
+            txtboxEmpId.Text = (entities.Emptables.Max(p => p.Eid)+1).ToString();
+            txtboxFname.Clear();
+            txtboxLname.Clear();
+            txtboxSalary.Clear();
+            foreach (var d in entities.DptTables)
+            {
+                cmbboxDeptId.Items.Add(d.DepartmentId);
+            }
+            cmbboxdesig.Items.AddRange(new string[] {"Programmer", "Maneger" ,"Sr Manegar", "Developer" });
+
+        }
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            if (txtboxEmpId.Text == "") //emp id will be auto generated 
+            {
+                ClearDataNew();
+            }
+            //check to make sure entry isnt null or an empty string
+            if (!string.IsNullOrWhiteSpace(txtboxFname.Text)&&!string.IsNullOrWhiteSpace(txtboxLname.Text) && !string.IsNullOrWhiteSpace(txtboxEmpId.Text)) 
+            {
+                Emptable obj = new Emptable();
+                obj.Eid = Convert.ToDecimal(txtboxEmpId.Text);
+                obj.EmpFirstName = txtboxFname.Text;
+                obj.EmpLastName = txtboxLname.Text;
+                obj.DepartmentId = Convert.ToDecimal(cmbboxDeptId.SelectedItem);
+                obj.Designation = cmbboxdesig.ToString();
+                obj.Salary = Convert.ToDecimal(txtboxSalary.Text);
+                entities.Emptables.Add(obj);//adds new obj to entity emptable 
+                entities.SaveChanges();//must call save ch;anges or it wont change in the server side
+                Clear();
+
+            }
+
+        }
+        private void Clear()
+        {// clears all the txt and combo boxes 
+            txtboxEmpId.Clear();
+            txtboxFname.Clear();
+            txtboxLname.Clear();
+            txtboxSalary.Clear();
+            cmbboxDeptId.Items.Clear();
+            cmbboxdesig.Items.Clear();
+        }
+
+        private void btnRefresh_Click(object sender, EventArgs e)
+        {
+            //makes the grid nothing , then makes the grid pop up with need changes made to database
+            gridviewEmp.DataSource = null;
+            gridviewEmp.DataSource = entities.Emptables.ToList();
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            var id = gridviewEmp.CurrentRow.Cells[0].Value; //selecting the row to delete
+            var emptodel = entities.Emptables.First(p => p.Eid == (decimal)id);// selecting the reference ,choosing the first query 
+            //which is employee id bc it is a unique value
+            entities.Emptables.Remove(emptodel);
+            entities.SaveChanges();
+        }
+
+        private void btnEdit_Click(object sender, EventArgs e)
+        {
+
+            //ability to click item and edit it
+            var id = gridviewEmp.CurrentRow.Cells[0].Value;
+            var emptoupdate = entities.Emptables.First(m => m.Eid == (decimal)id);//getting reference of the object selected 
+            //populating everythinng here into the text boxes
+            txtboxEmpId.Text = emptoupdate.Eid.ToString();
+            txtboxEmpId.ReadOnly = true;
+            txtboxFname.Text = emptoupdate.EmpFirstName;
+            txtboxLname.Text = emptoupdate.EmpLastName;
+            //populate the 
+            foreach ( var d in entities.DptTables)
+            {
+                cmbboxDeptId.Items.Add(d.DepartmentId);
+            }
+            cmbboxDeptId.SelectedItem = emptoupdate.DepartmentId;
+
+            cmbboxdesig.Items.AddRange(new string[] { "Programmer", "Maneger", "Sr Manegar", "Developer" });
+            cmbboxdesig.SelectedItem = emptoupdate.Designation;
+            txtboxSalary.Text = emptoupdate.Salary.ToString();
+            btnUpdate.Enabled = true;
+
+
+
+        }
+
+        private void btnUpdate_Click(object sender, EventArgs e)
+        {
+            //this actually updated the whole object/row 
+            var id = Convert.ToDecimal(txtboxEmpId.Text);
+            var emptoupdate = entities.Emptables.First(m => m.Eid == id);//getting reference of the object selected,need to do 
+            //again
+            emptoupdate.EmpFirstName = txtboxFname.Text;
+            emptoupdate.EmpLastName = txtboxLname.Text;
+            emptoupdate.DepartmentId = Convert.ToDecimal(cmbboxDeptId.SelectedItem);
+            emptoupdate.Designation = cmbboxdesig.SelectedItem.ToString();
+            emptoupdate.Salary = decimal.Parse(txtboxSalary.Text);
+           // entities.Emptables.Add(emptoupdate);//adds new obj to entity emptable//// dont need bc its already in the list just 
+            entities.SaveChanges();
+            Clear();
+
+
+        }
+    }
+}
